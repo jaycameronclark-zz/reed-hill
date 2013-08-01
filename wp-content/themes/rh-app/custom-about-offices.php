269bd97bfe907f770bd9c2bebef6ab420ad8bb08
jaycameronclark <jaycameronclark@gmail.com>
2013-07-31 21:54:20 -0600

    Update EFW images to latest. Populated the rest of the locations

--- wp-content/themes/rh-app/custom-about-offices.php
+++ wp-content/themes/rh-app/custom-about-offices.php
@@ -21,9 +21,21 @@ Template Name: Offices
 
 					<span class="star denver"><i class="title">Denver, CO</i></span>
 					<span class="star phoenix"><i class="title">Phoenix, AZ</i></span>
-					<span class="dot mexico"><i class="title">Location</i></span>
-					<span class="dot sa-1"><i class="title">Location</i></span>
-					<span class="dot sa-2"><i class="title">Location</i></span>
+					<span class="dot mexico"><i class="title">Mexico</i></span>
+					<span class="dot sa-1"><i class="title">ARGENTINA</i></span>
+					<span class="dot sa-2"><i class="title">Brazil</i></span>
+					<span class="dot so-af-1"><i class="title">South Africa</i></span>
+					<span class="dot aust"><i class="title">Australia</i></span>
+					<span class="dot uk"><i class="title">UK</i></span>
+					<span class="dot ger"><i class="title">Germany</i></span>
+					<span class="dot swed"><i class="title">Sweden</i></span>
+					<span class="dot russ"><i class="title">Russia</i></span>
+					<span class="dot uae"><i class="title">uae</i></span>
+					<span class="dot india"><i class="title">India</i></span>
+					<span class="dot sing"><i class="title">Singapore</i></span>
+					<span class="dot china"><i class="title">China</i></span>
+					<span class="dot korea"><i class="title">Korea</i></span>
+					<span class="dot japan"><i class="title">Japan</i></span>
 				</div>
 			</figure>
 		</section>
