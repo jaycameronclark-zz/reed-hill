ba226d9deb27ef75646999ced6255e2353a75293
jaycameronclark <jaycameronclark@gmail.com>
2013-08-01 22:17:33 -0600

    Built out the Shop pages with content.

--- wp-content/themes/rh-app/page.php
+++ wp-content/themes/rh-app/page.php
@@ -1,51 +1,51 @@
-<!-- ////////// PAGE.PHP /// -->
+<?php
+/*
+Template Name: Offices >> Denver
+*/
+?>
 <?php get_header(); ?>
 
+<script>
+  $(function(){
+
+    $(".contact-modal").click(function(){
+      $("#contact-modal").modal("show");
+    });
+
+  });
+</script>
+
 <div class="grid--full">
-  <section class="main-container min-height" id="<?php the_ID();?>">
-<?php echo "<!--"; ?>
-
-
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-<?php $args = array(
-  'sort_order' => 'ASC',
-  'sort_column' => 'post_title',
-  'hierarchical' => 0,
-  'exclude' => '',
-  'include' => '',
-  'meta_key' => '',
-  'meta_value' => '',
-  'authors' => '',
-  'child_of' => 0,
-  'parent' => $post->ID,
-  'exclude_tree' => '',
-  'number' => '',
-  'offset' => 0,
-  'post_type' => 'page',
-  'post_status' => 'publish'
-); 
-
-  $subpages = get_pages($args);
-
-  foreach( $subpages as $page ) {    
-    $content = $page->post_content;
-
-    $content = apply_filters( 'the_content', $content );
-
-  ?>
-    <?php echo "-->"; ?><a href="<?php echo get_page_link( $page->ID ); ?>"><article class="grid__item one-quarter palm-one-half tile">
-    <?php echo get_the_post_thumbnail($page->ID, 'client-thumb'); ?> 
-    <div class="title one-whole"><?php echo $page->post_title; ?></div>
-    </article></a><?php echo "<!--"; ?>
-  <?php } ?>
-
-<?php endwhile; ?>
-<?php endif; ?>
-<?php echo "-->"; ?>
-  </section>
+    <section class="main-container" id="post-<?php the_ID(); ?>">
+      <figure class="grid__item one-whole palm-one-whole offices">
+        <div class="map-grid">
+          <div class="grid__item one-whole palm-one-whole">
+          <article class="content soft one-whole">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+              <span class="arrow"></span><?php the_content(); ?>
+            <?php endwhile; ?>
+            <?php endif; ?>
+          </article>
+          </div>
+          <img src="<?php bloginfo( 'template_directory' ); ?>/_/images/denver-inner-bg.jpg" alt="">
+        </div>
+      </figure>
+    </section>
 </div>
+ 
+<!-- Modal -->
+<div id="contact-modal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-header">
+    <span class="close" data-dismiss="modal" aria-hidden="true"></span>
+    <h3 id="myModalLabel"></h3>
+  </div>
+  <div class="modal-body">
+    <p>Form goes here..</p>
+  </div>
+  <div class="modal-footer">
 
+  </div>
+</div>
 
 
 <?php get_footer(); ?>
